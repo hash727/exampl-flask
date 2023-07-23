4b1203563751fc4219deb60bb172b5e802580050
Harish Kumar <pharishkumar123@gmail.com>
2023-07-23 16:56:55 +0530

    Changes added

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return "<h1> Hello World.!, by HasH from local machine </h1>"
+    return "<h1> Hello World.!, by HasH from my local machine </h1>"
 
 if __name__ == "__main__":
     application.run(port=8000, debug=False)
